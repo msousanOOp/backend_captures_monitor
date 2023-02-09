b7686659ea6365949791b2dbb3c08bcc43495c82
Rodrigo Danieli <rodri.merlucci@gmail.com>
2023-02-08 21:43:03 -0300

    Update Ssh.php

--- src/Connectors/Ssh.php
+++ src/Connectors/Ssh.php
@@ -36,12 +36,12 @@ class Ssh extends \App\Connector
                     self::$keys[$this->valid_key] = PublicKeyLoader::load($pass);
                 }
                 
-                if(!self::$connections[$this->valid_key]->isConnected())
-                {
-                    self::$connections[$this->valid_key]->reconnect();
-                }
+                // if(!self::$connections[$this->valid_key]->isConnected())
+                // {
+                //     self::$connections[$this->valid_key]->reconnect();
+                // }
                
-                if (!self::$connections[$this->valid_key]->login($user, self::$keys[$this->valid_key])) {
+                if (!self::$connections[$this->valid_key]->ping() && !self::$connections[$this->valid_key]->login($user, self::$keys[$this->valid_key])) {
                     unset(self::$connections[$this->valid_key]);
                     throw new \Exception(self::$connections[$this->valid_key]->getLastError());
                 }
